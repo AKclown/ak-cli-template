6c8ed03788fb744a54d6578cda09c4bf94350a3e
AKclown <13413629530@139.com>
2022-02-16 18:11:30 +0800

    vue模板创建

---
+++ ak-cli-template-vue2/template/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">vue</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
